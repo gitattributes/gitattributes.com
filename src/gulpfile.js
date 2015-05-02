a0ccc506f74b8b9b96feaef9b316dc1c0c953238
Jozef Izso <jozef.izso@gmail.com>
2015-05-02 16:56:31 +0200

    Added web project files.

---
+++ src/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+  del = require("del"),
+  fs = require("fs");
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+  bower: "./bower_components/",
+  lib: "./" + project.webroot + "/lib/"
+};
+
+gulp.task("clean", function (cb) {
+  del([paths.lib], cb);
+});
+
