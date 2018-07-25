9734e7a741875e693a3f00ab6b4f3645c65ea467
Jozef Izso <jozef.izso@gmail.com>
2018-07-25 14:04:15 +0200

    Remove bower

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,24 +3,16 @@
 var gulp = require("gulp"),
   del = require("del"),
   fs = require("fs");
-var bower = require("main-bower-files");
-var bowerNormalize = require("gulp-bower-normalize");
 
 var paths = {
-  bower: "./bower_components/",
-  lib: "./wwwroot/lib/",
   data: "./wwwroot/data/"
 };
 
 gulp.task("clean", function () {
-  return del([paths.lib, paths.data]);
+  return del([paths.data]);
 });
 
 gulp.task("copy", ["clean"], function () {
   gulp.src('./data/gitattributes/*.gitattributes')
    .pipe(gulp.dest(paths.data));
-
-  return gulp.src(bower(), { base: paths.bower })
-    .pipe(bowerNormalize())
-    .pipe(gulp.dest(paths.lib));
 });
