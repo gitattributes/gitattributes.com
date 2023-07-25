3eea04f7c375474bae81a91d9b452dfe96575a8a
Jozef Izso <jozef.izso@gmail.com>
2023-07-25 23:32:49 +0200

    Create website using Vite

---
+++ web/site.js
@@ -0,0 +1,40 @@
+﻿import 'bootswatch/paper/bootstrap.css'
+import '@selectize/selectize/dist/css/selectize.css'
+import './site.css'
+
+import $ from 'jquery'
+import {} from '@selectize/selectize'
+
+$(document).ready(function() {
+  var ignores = JSON.parse($('script#ignores-data').html());
+  var types = $('#types');
+  types.selectize({
+    plugins: ['restore_on_backspace', 'remove_button'],
+    inputClass: 'form-control selectize-input',
+    options: ignores,
+    placeholder: 'Select file types to generate gitattributes file',
+    valueField: 'id',
+    labelField: 'name',
+    searchField: ['name'],
+    delimiter: ',',
+    persist: false,
+    create: false,
+    selectOnTab: true
+  });
+
+  $('#btnGenerate').on('click', function() {
+    var typesValue = types.val();
+    if (typesValue.length) {
+      var uriEncodedFiles = encodeURIComponent(typesValue);
+      window.location="/api/"+uriEncodedFiles;
+    }
+  });
+
+  $('#btnDownload').on('click', function() {
+    var typesValue = types.val();
+    if (typesValue.length) {
+      var uriEncodedFiles = encodeURIComponent(typesValue);
+      window.location="/api/f/"+uriEncodedFiles;
+    }
+  });
+});
